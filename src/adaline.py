09d832117d4f3c507cd24a13ae92cf38ae53e6b4
Shota Shimizu <s_shimizu@pkshatech.com>
2020-01-22 21:06:43 +0900

    p.48 functionality to adaline

--- src/adaline.py
+++ src/adaline.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class AdalineGD:
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+    def __init__(self, eta=0.01, n_iter=50, shuffle=True, random_state=None):
         """Adaptive LInear NEuron classifier.
 
         Parameters
@@ -22,6 +22,8 @@ class AdalineGD:
         """
         self.eta = eta
         self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
         self.random_state = random_state
 
     def fit(self, X, y):
@@ -38,19 +40,50 @@ class AdalineGD:
         -------
         self
         """
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self._initalize_weights(X.shape[1])
         self.cost_ = []
 
         for i in range(self.n_iter):
-            net_input = self.net_input(X)
-            output = self.activation(net_input)
-            errors = y - output
-            self.w_[1:] += self.eta * X.T.dot(errors)
-            self.w_[0] += self.eta * errors.sum()
-            cost = (errors ** 2).sum() / 2.0
-            self.cost_.append(cost)
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+
         return self
+
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """Initialize weights to small random numbers"""
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01, size=1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.activation(self.net_input(xi))
+        error = target - output
+        self.w_[1:] += self.eta * X.T.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
 
     def net_input(self, X):
         """Calculate net input"""
